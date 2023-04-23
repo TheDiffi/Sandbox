cf8dd76290d12bf641c6d7db4a94c0dde0337be9
TheDiffi <diffi.penn@gmail.com>
2023-04-23 23:27:56 +0200

    randomSite improvment

--- 1.template/index.js
+++
@@ -1,6 +0,0 @@
-function randred(){
-    // read the file at the path and return a random line
-    var file = path.join(__dirname, 'data.txt');
-    console.log(file);
-    
-}
--- redirect.js
+++ redirect.js
@@ -1,5 +1,12 @@
 //include sites to be displayed in the sandbox here
-sites = ["cats", "text_mask"]
+sites = ["cats", "text_mask"];
+
+// Get the name of the current project folder
+const lastSite = window.location.search.substring(1);
+
+// Remove the current project from the projects array
+const availableSites = sites.filter((sites) => sites !== lastSite);
+
 function randSandboxSite() {
-    return "./" + sites[Math.floor(Math.random() * sites.length)];
+	return "./" + availableSites[Math.floor(Math.random() * (availableSites.length - 0.1))];
 }
