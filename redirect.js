cabd684b28c48722bc188adbe7ebec6abf60092e
Diffi <diffi.penn@gmail.com>
2023-10-28 19:37:08 +0000

    sandbox

--- redirect.js
+++ redirect.js
@@ -10,3 +10,18 @@ const availableSites = sites.filter((sites) => sites !== lastSite);
 function randSandboxSite() {
 	return "./" + availableSites[Math.floor(Math.random() * (availableSites.length - 0.1))];
 }
+
+
+function injectFooter(body){
+	// Load the footer HTML from the "footer.html" file
+	fetch("./footer.html")
+	.then((response) => response.text())
+	.then((html) => {
+		// Append the footer HTML to the body of the page
+		body.insertAdjacentHTML("beforeend", html);
+	})
+	.catch((error) => {
+		// Handle any errors that occur while loading the footer
+		console.error(error);
+	});
+}
