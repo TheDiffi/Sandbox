36908b5474f91b11e8894ee9b90db33ed3d5288b
TheDiffi <64838662+TheDiffi@users.noreply.github.com>
2023-10-30 13:20:02 +0100

    parallaxe scroll slideshow

---
+++ parallax/index.js
@@ -0,0 +1,33 @@
+let slideshow = document.getElementById("slideshow");
+
+window.onmousedown = function (e) {
+	let startX = e.clientX;
+	let lastPos = parseFloat(slideshow.dataset.lastPos);
+    let newPos = lastPos;
+
+	window.onmousemove = function (e) {
+		let deltaX = e.clientX - startX;
+		let relativeMove = (deltaX / window.innerWidth) * 100;
+		newPos = Math.max(Math.min(lastPos + relativeMove * 2, 100), -100);
+
+		slideshow.style.left = newPos + "%";
+        console.log(newPos);
+		/* slideshow.animate(
+			{
+				left: newPos + "%",
+			},
+			{ duration: 800, fill: "forwards", easing: "ease-out" }
+		); */
+
+		for (const image of slideshow.getElementsByClassName("image")) {
+			image.style.objectPosition = (newPos + 100) / 2 + "% 50%";
+		}
+	};
+
+	window.onmouseup = function () {
+		window.onmousemove = null;
+		window.onmouseup = null;
+
+        slideshow.dataset.lastPos = newPos || 50;
+	};
+};
--- redirect.js
+++ redirect.js
@@ -1,5 +1,5 @@
 //include sites to be displayed in the sandbox here
-sites = ["cats", "text_mask"];
+sites = ["cats", "text_mask", "parallax"];
 
 // Get the name of the current project folder
 const lastSite = window.location.search.substring(1);
@@ -9,4 +9,11 @@ const availableSites = sites.filter((sites) => sites !== lastSite);
 
 function randSandboxSite() {
 	return "./" + availableSites[Math.floor(Math.random() * (availableSites.length - 0.1))];
+}
+
+function nextSandboxSite() {
+	// get the index of the current project
+	const currentIndex = availableSites.indexOf(lastSite) || 0;
+
+	return "./" + availableSites[currentIndex + 1];
 }
